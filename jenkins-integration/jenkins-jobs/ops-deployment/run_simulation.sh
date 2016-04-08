e8101bbd48e202448737977aa2932184eec1fb34
Chris Price <chris@puppetlabs.com>
2016-04-08 08:56:26 -0700

    WIP: remove ruby code from bash script (facepalm)

--- jenkins-integration/jenkins-jobs/ops-deployment/run_simulation.sh
+++ jenkins-integration/jenkins-jobs/ops-deployment/run_simulation.sh
@@ -4,6 +4,6 @@ set -x
 set -e
 
 pushd ../simulation-runner
-PUPPET_GATLING_MASTER_BASE_URL=https://#{$PUPPET_GATLING_MASTER_BASE_URL}:8140 sbt run
+PUPPET_GATLING_MASTER_BASE_URL=https://$PUPPET_GATLING_MASTER_BASE_URL:8140 sbt run
 popd
 
