4847bab96e86fa469035ceeeb794e7f3ddbeaf7f
Chris Price <chris@puppetlabs.com>
2016-04-10 10:47:56 -0700

    WIP: update hostgen to cent7

--- jenkins-integration/jenkins-jobs/ops-deployment/setup_master.sh
+++ jenkins-integration/jenkins-jobs/ops-deployment/setup_master.sh
@@ -30,8 +30,8 @@ ssh-add ${HOME}/.ssh/id_rsa*
 #          | sed -e 's/hypervisor: vmpooler/hypervisor: none/1' \
 #          > hosts.yaml
 pe_version=2015.3.1 pe_family=2015.3.1 \
-        /usr/local/bin/bundle exec beaker-hostgenerator centos6-64mdca \
-        | sed -e "s/centos6-64-1/$PUPPET_GATLING_MASTER_BASE_URL/1" \
+        /usr/local/bin/bundle exec beaker-hostgenerator centos7-64mdca \
+        | sed -e "s/centos7-64-1/$PUPPET_GATLING_MASTER_BASE_URL/1" \
         | sed -e 's/hypervisor: vmpooler/hypervisor: none/1' \
         > hosts.yaml
 
