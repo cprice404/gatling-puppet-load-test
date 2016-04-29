cc9273330f9eea03138262d80333794912052d55
Chris Price <chris@puppetlabs.com>
2016-04-28 21:42:06 -0700

    WIP: getting ruby working

--- jenkins-integration/jenkins-jobs/ops-deployment/setup_master.sh
+++ jenkins-integration/jenkins-jobs/ops-deployment/setup_master.sh
@@ -18,6 +18,15 @@ set -e
 eval $(ssh-agent -t 24h -s)
 ssh-add ${HOME}/.ssh/id_rsa
 
+echo ""
+echo "Setting Up Ruby Environment"
+echo ""
+. /usr/local/rvm/scripts/rvm
+rvm use 2.1.6 || exit 1
+rvm list
+export GEM_SOURCE=http://rubygems.delivery.puppetlabs.net
+echo -e "\n\n\n"
+
 which ruby
 which bundle
 
