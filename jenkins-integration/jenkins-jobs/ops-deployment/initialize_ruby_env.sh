00216a612c9d0d400c8e03e292bb575ff379f298
Chris Price <chris@puppetlabs.com>
2016-04-29 12:40:33 -0700

    WIP: separating server auth from gatling auth

--- jenkins-integration/jenkins-jobs/ops-deployment/initialize_ruby_env.sh
+++ jenkins-integration/jenkins-jobs/ops-deployment/initialize_ruby_env.sh
@@ -6,7 +6,7 @@ set -x
 echo ""
 echo "Setting Up Ruby Environment"
 echo ""
-source /usr/local/rvm/scripts/rvm
+. /usr/local/rvm/scripts/rvm
 rvm use 2.1.6 || exit 1
 rvm list
 export GEM_SOURCE=http://rubygems.delivery.puppetlabs.net
