9add8bf4eea5231078b039cbdb01f6153551ea75
Chris Price <chris@puppetlabs.com>
2016-05-12 15:02:29 -0700

    WIP: trying out Jenkinsfile

--- jenkins-integration/jenkins-jobs/job_bootstrap.groovy
+++ jenkins-integration/jenkins-jobs/job_bootstrap.groovy
@@ -1,31 +1,52 @@
 import groovy.io.FileType
-//
-//def list = []
-//
+
+def repo = 'git://10.0.19.111/gatling-puppet-load-test'
+def branch = 'scratch/master/pipeline-test'
+
 def dir = new File(".")
-//dir.eachFileRecurse (FileType.FILES) { file ->
-//    list << file
-//}
+dir.eachFileRecurse (FileType.FILES) { file ->
+//    println "FILE: " + file
+    if (file.name.equals("Jenkinsfile")) {
+        println "FOUND A JANKFILE: " + file
+        println "Parent dir:" + file.parentFile.name
+        job_prefix = file.parentFile.name
+        workflowJob('thabootstrap') {
+            definition {
+                cpsScm {
+                    scm {
+                        git {
+                            remote {
+                                url(repo)
+                            }
+                            branch(branch)
+                        }
+                    }
+                    scriptPath(file)
+                }
+            }
+        }
+    }
+}
 //
 //list.each {
 //    println it.path
 //}
 
-println "FOOOOO!: " + dir
+//println "FOOOOO!: " + dir
 
 
-workflowJob('thabootstrap') {
-    definition {
-        cpsScm {
-            scm {
-                git {
-                    remote {
-                        url('git://10.0.19.111/gatling-puppet-load-test')
-                    }
-                    branch('scratch/master/pipeline-test')
-                }
-            }
-            scriptPath('Jenkinsfile')
-        }
-    }
-}
+//workflowJob('thabootstrap') {
+//    definition {
+//        cpsScm {
+//            scm {
+//                git {
+//                    remote {
+//                        url('git://10.0.19.111/gatling-puppet-load-test')
+//                    }
+//                    branch('scratch/master/pipeline-test')
+//                }
+//            }
+//            scriptPath('Jenkinsfile')
+//        }
+//    }
+//}
