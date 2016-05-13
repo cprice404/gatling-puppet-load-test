6f69ccaa6dadce8ccc3f2a3a765d539241e1842a
Chris Price <chris@puppetlabs.com>
2016-05-13 08:13:20 -0700

    WIP: working on pipeline stuff

--- jenkins-integration/jenkins-jobs/common/scripts/pipeline.groovy
+++ jenkins-integration/jenkins-jobs/common/scripts/pipeline.groovy
@@ -1,4 +1,14 @@
 def build() {
     echo 'the pipeline, boss'
+    stage '010-provision-sut'
+    stage '020-install-pe'
+    stage '030-customize-settings'
+    stage '040-install-puppet-code'
+    stage '050-file-sync'
+    stage '060-classify-nodes'
+    stage '070-validate-classification'
+    stage '080-launch-bg-script'
+    stage '090-run-gatling-sim'
+    stage '100-collect-artifacts'
 }
 return this;
