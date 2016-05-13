3d17e226e598da5fa027f2e54c540d8ff28ce6dd
Chris Price <chris@puppetlabs.com>
2016-05-13 09:38:17 -0700

    WIP: working on pipeline stuff

--- jenkins-integration/jenkins-jobs/common/scripts/020_install_pe.sh
+++ jenkins-integration/jenkins-jobs/common/scripts/020_install_pe.sh
@@ -13,14 +13,16 @@ set -e
 eval $(ssh-agent -t 24h -s)
 ssh-add ${HOME}/.ssh/id_rsa
 
+# TODO: get rid of references to ops-deployment
+
 bundle exec beaker \
         --config hosts.yaml \
         --load-path lib \
         --log-level debug \
         --no-color \
         --tests \
-jenkins-jobs/ops-deployment/hack_hostname_into_etc_hosts.rb,\
-jenkins-jobs/ops-deployment/disable_firewall.rb,\
+jenkins-jobs/scenarios/ops-deployment/hack_hostname_into_etc_hosts.rb,\
+jenkins-jobs/scenarios/ops-deployment/disable_firewall.rb,\
 beaker/install/pe/10_install_pe.rb
 
 # without this set +x, rvm will log 10 gigs of garbage
