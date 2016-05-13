2d8d53c41ce5e97ad9113095f4b155d6f7c6c825
Chris Price <chris@puppetlabs.com>
2016-05-13 12:58:45 -0700

    WIP: working on pipeline stuff

--- jenkins-integration/jenkins-jobs/common/scripts/090_run_simulation.sh
+++ jenkins-integration/jenkins-jobs/common/scripts/090_run_simulation.sh
@@ -2,22 +2,30 @@
 
 set -x
 set -e
-#
-## TODO: this relies on knowledge of the CWD.  Fix it to figure out the path
-##  of the currently executing script and use that as the base path so that this
-##  script will be more re-usable.
+
+pushd jenkins-integration
+source jenkins-jobs/ops-deployment/initialize_ruby_env.sh
+
+# This job sets up the following:
+# - local keystore file that gatling can use to talk to the SUT
+# - executes gatling simulation
+
+bundle exec beaker \
+        --config hosts.yaml \
+        --load-path lib \
+        --log-level debug \
+        --no-color \
+        --tests \
+beaker/install/shared/configure_gatling_auth.rb
 
 
-#source jenkins-jobs/ops-deployment/initialize_ruby_env.sh
+# without this set +x, rvm will log 10 gigs of garbage
+set +x
+popd
 
-#bundle exec beaker \
-#        --config hosts.yaml \
-#        --load-path lib \
-#        --log-level debug \
-#        --no-color \
-#        --tests \
-#beaker/install/shared/configure_gatling_auth.rb
-
+set -x
 pushd simulation-runner
 PUPPET_GATLING_MASTER_BASE_URL=https://$PUPPET_GATLING_MASTER_BASE_URL:8140 sbt run
+# without this set +x, rvm will log 10 gigs of garbage
+set +x
 popd
