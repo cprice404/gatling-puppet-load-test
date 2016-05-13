78bad8eaace10d87731219b5f2b5a02ba5cfbfe3
Chris Price <chris@puppetlabs.com>
2016-05-13 13:33:35 -0700

    WIP: working on pipeline stuff

--- jenkins-integration/jenkins-jobs/common/scripts/pipeline.groovy
+++ jenkins-integration/jenkins-jobs/common/scripts/pipeline.groovy
@@ -27,7 +27,7 @@ def build(git_url, git_branch, job_name) {
         sh "${script_dir}/040_install_puppet_code.sh"
 
         stage '050-file-sync'
-        //sh "${script_dir}/050_file_sync.sh"
+        sh "${script_dir}/050_file_sync.sh"
 
         stage '060-classify-nodes'
         withEnv(["PUPPET_GATLING_SIMULATION_CONFIG=${PUPPET_GATLING_SIMULATION_CONFIG}"]) {
