ae59d3f0a54fce3b387bf2bd1acca1026a1f0dae
Chris Price <chris@puppetlabs.com>
2016-05-13 11:25:56 -0700

    WIP: working on pipeline stuff

--- jenkins-integration/jenkins-jobs/common/scripts/040_install_puppet_code.sh
+++ jenkins-integration/jenkins-jobs/common/scripts/040_install_puppet_code.sh
@@ -28,8 +28,8 @@ bundle exec beaker \
         --log-level debug \
         --no-color \
         --tests \
-jenkins-jobs/scenarios/ops-deployment/install_large_files.rb,\
-jenkins-jobs/scenarios/ops-deployment/install_ops_modules_snapshot.rb
+jenkins-jobs/scenarios/ops-deployment/install_ops_modules_snapshot.rb,\
+jenkins-jobs/scenarios/ops-deployment/install_catalog_zero.rb
 
 # without this set +x, rvm will log 10 gigs of garbage
 set +x
