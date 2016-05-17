4c897170784e5bfda3478ae1a16ad32397bef0b2
Chris Price <chris@puppetlabs.com>
2016-05-17 13:57:20 -0700

    WIP: script to use for testing gatling plugin

--- simulation-runner/FOO_run_scenario.sh
+++ simulation-runner/FOO_run_scenario.sh
@@ -2,5 +2,8 @@
 
 pushd simulation-runner
 echo "sup, yo"
-sbt run
+PUPPET_GATLING_SIMULATION_CONFIG=./config/scenarios/ops-scenario.json \
+   PUPPET_GATLING_SIMULATION_ID=foo \
+   PUPPET_GATLING_MASTER_BASE_URL=http://localhost:66666 \
+   sbt run
 popd
