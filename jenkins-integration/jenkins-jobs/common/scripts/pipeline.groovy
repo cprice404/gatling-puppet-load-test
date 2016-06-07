47b60eaab6fa281537395152702d314e65657384
Chris Price <chris@puppetlabs.com>
2016-06-07 12:33:51 -0700

    WIP: pulled all steps out into methods

--- jenkins-integration/jenkins-jobs/common/scripts/pipeline.groovy
+++ jenkins-integration/jenkins-jobs/common/scripts/pipeline.groovy
@@ -4,8 +4,59 @@ def step000_provision_sut() {
     echo "Hi from new step000 method! TODO: I should be provisioning your SUT, but I'm not."
 }
 
+def step010_setup_beaker(script_dir) {
+    withEnv(["SUT_HOST=${SUT_HOST}"]) {
+        sh "${script_dir}/010_setup_beaker.sh"
+    }
+}
 
-def build(job_name) {
+def step020_install_pe(SKIP_PE_INSTALL, script_dir) {
+    echo "SKIP PE INSTALL?: ${SKIP_PE_INSTALL} (${SKIP_PE_INSTALL.class})"
+    if (SKIP_PE_INSTALL) {
+        echo "Skipping PE install because SKIP_PE_INSTALL is set."
+    } else {
+        sh "${script_dir}/020_install_pe.sh"
+    }
+}
+
+def step030_customize_settings() {
+    echo "Hi! TODO: I should be customizing PE settings on the SUT, but I'm not."
+}
+
+def step040_install_puppet_code(script_dir) {
+    sh "${script_dir}/040_install_puppet_code.sh"
+}
+
+def step050_file_sync(script_dir) {
+    sh "${script_dir}/050_file_sync.sh"
+}
+
+def step060_classify_nodes(script_dir) {
+    withEnv(["PUPPET_GATLING_SIMULATION_CONFIG=${PUPPET_GATLING_SIMULATION_CONFIG}"]) {
+        sh "${script_dir}/060_classify_nodes.sh"
+    }
+}
+
+def step070_classify_nodes() {
+    echo "Hi! TODO: I should be validating classification on your SUT, but I'm not."
+}
+
+def step080_launch_bg_scripts() {
+    echo "Hi! TODO: I should be launching background scripts on your SUT, but I'm not."
+}
+
+def step090_run_gatling_sim(job_name, script_dir) {
+    withEnv(["PUPPET_GATLING_SIMULATION_CONFIG=${PUPPET_GATLING_SIMULATION_CONFIG}",
+             "PUPPET_GATLING_SIMULATION_ID=${job_name}"]) {
+        sh "${script_dir}/090_run_simulation.sh"
+    }
+}
+
+def step100_collect_artifacts() {
+    gatlingArchive()
+}
+
+def single_pipeline(job_name) {
     def script_dir = "./jenkins-integration/jenkins-jobs/common/scripts"
 
     node {
@@ -21,51 +72,42 @@ def build(job_name) {
 //        step([$class: 'GatlingBuildAction'])
 
         stage '010-setup-beaker'
-        withEnv(["SUT_HOST=${SUT_HOST}"]) {
-            sh "${script_dir}/010_setup_beaker.sh"
-        }
+        step010_setup_beaker(script_dir)
 
         stage '020-install-pe'
-        echo "SKIP PE INSTALL?: ${SKIP_PE_INSTALL} (${SKIP_PE_INSTALL.class})"
-        if (SKIP_PE_INSTALL) {
-            echo "Skipping PE install because SKIP_PE_INSTALL is set."
-        } else {
-            sh "${script_dir}/020_install_pe.sh"
-        }
+        step020_install_pe(SKIP_PE_INSTALL, script_dir)
 
         stage '030-customize-settings'
-        echo "Hi! TODO: I should be customizing PE settings on the SUT, but I'm not."
+        step030_customize_settings()
 
         stage '040-install-puppet-code'
-        sh "${script_dir}/040_install_puppet_code.sh"
+        step040_install_puppet_code(script_dir)
 
         stage '050-file-sync'
-        sh "${script_dir}/050_file_sync.sh"
+        step050_file_sync(script_dir)
 
         stage '060-classify-nodes'
-        withEnv(["PUPPET_GATLING_SIMULATION_CONFIG=${PUPPET_GATLING_SIMULATION_CONFIG}"]) {
-            sh "${script_dir}/060_classify_nodes.sh"
-        }
+        step060_classify_nodes(script_dir)
 
         stage '070-validate-classification'
-        echo "Hi! TODO: I should be validating classification on your SUT, but I'm not."
+        step070_classify_nodes()
 
-        stage '080-launch-bg-script'
-        echo "Hi! TODO: I should be launching background scripts on your SUT, but I'm not."
+        stage '080-launch-bg-scripts'
+        step080_launch_bg_scripts()
 
         stage '090-run-gatling-sim'
-        withEnv(["PUPPET_GATLING_SIMULATION_CONFIG=${PUPPET_GATLING_SIMULATION_CONFIG}",
-                 "PUPPET_GATLING_SIMULATION_ID=${job_name}"]) {
-            sh "${script_dir}/090_run_simulation.sh"
-        }
+        step090_run_gatling_sim(job_name, script_dir)
 
         stage '100-collect-artifacts'
-        gatlingArchive()
-        echo "Hi! TODO: I should be collecting the final job artifacts, but I'm not."
+        step100_collect_artifacts()
+//        echo "Hi! TODO: I should be collecting the final job artifacts, but I'm not."
 //        step([$class: 'JUnitResultArchiver', testResults: '**/target/surefire-reports/TEST-*.xml'])
 //        step([$class: 'GatlingBuildAction'])
 
     }
 
 }
+
+
+
 return this;
