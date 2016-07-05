235a17fa01e770e162ec2158ec47c1b1dbc2f175
Chris Price <chris@puppetlabs.com>
2016-07-05 11:57:01 -0700

    WIP: fix environment

--- jenkins-integration/dev/beaker/40_apply_role.rb
+++ jenkins-integration/dev/beaker/40_apply_role.rb
@@ -2,5 +2,5 @@ test_name "Apply puppet role for driver node in dev env"
 
 step "Apply role on dev machine" do
   # TODO: docs here about how to set environment for dev
-  on(jenkins, puppet("apply", "--environment", "maint/master/add-gatling-plugin", "-e", "'include ::puppetserver_perf_driver::role::puppetserver::perf::driver::dev'"))
+  on(jenkins, puppet("apply", "--environment", "maint_master_add_gatling_plugin", "-e", "'include ::puppetserver_perf_driver::role::puppetserver::perf::driver::dev'"))
 end
