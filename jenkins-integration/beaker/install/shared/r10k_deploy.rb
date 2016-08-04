9f05e8e9aa88987077403e5bd8273a6994e33ee2
Chris Price <chris@puppetlabs.com>
2016-08-04 12:40:30 -0700

    WIP: working on r10k deploy

--- jenkins-integration/beaker/install/shared/r10k_deploy.rb
+++ jenkins-integration/beaker/install/shared/r10k_deploy.rb
@@ -66,7 +66,7 @@ end
 step "add github to known hosts" do
   # Create known_hosts file with GitHub host key to prevent
   # "Host key verification failed" errors during clones
-  create_remote_file(jenkins, "/root/.ssh/known_hosts", <<-EOS)
+  create_remote_file(master, "/root/.ssh/known_hosts", <<-EOS)
   github.com ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAq2A7hRGmdnm9tUDbO9IDSwBK6TbQa+PXYPCPy6rbTrTtw7PHkccKrpp0yVhp5HdEIcKr6pLlVDBfOLX9QUsyCOV0wzfjIJNlGEYsdlLJizHhbn2mUjvSAHQqZETYP81eFzLQNnPHt4EVVUh7VfDESU84KezmD5QlWpXLmvU31/yMf+Se8xhHTvKSCZIFImWwoG6mbUoWf9nzpIoaSjB+weqqUUmpaaasXVal72J+UX2B+2RPW3RcT0eOzQgqlJL3RKrTJvdsjE3JEAvGq3lGHSZXy28G3skua2SmVi/w4yCE6gbODqnTWlg7+wC604ydGXA8VJiS5ap43JXiUFFAaQ==
   EOS
 end
