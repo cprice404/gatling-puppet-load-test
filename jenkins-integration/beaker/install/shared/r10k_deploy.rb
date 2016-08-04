57c2a6b2b0434be7288bfe1f5d55e0b5bd226ad4
Chris Price <chris@puppetlabs.com>
2016-08-04 12:13:44 -0700

    WIP: working on r10k deploy

--- jenkins-integration/beaker/install/shared/r10k_deploy.rb
+++ jenkins-integration/beaker/install/shared/r10k_deploy.rb
@@ -43,6 +43,11 @@ def run_r10k_deploy(host, r10k_config)
   end
 end
 
+
+create_remote_file(master, '/root/.ssh/known_hosts', <<-EOF)
+github.com ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAq2A7hRGmdnm9tUDbO9IDSwBK6TbQa+PXYPCPy6rbTrTtw7PHkccKrpp0yVhp5HdEIcKr6pLlVDBfOLX9QUsyCOV0wzfjIJNlGEYsdlLJizHhbn2mUjvSAHQqZETYP81eFzLQNnPHt4EVVUh7VfDESU84KezmD5QlWpXLmvU31/yMf+Se8xhHTvKSCZIFImWwoG6mbUoWf9nzpIoaSjB+weqqUUmpaaasXVal72J+UX2B+2RPW3RcT0eOzQgqlJL3RKrTJvdsjE3JEAvGq3lGHSZXy28G3skua2SmVi/w4yCE6gbODqnTWlg7+wC604ydGXA8VJiS5ap43JXiUFFAaQ==
+EOF
+
 r10k_config = get_r10k_config_from_env()
 if r10k_config
   install_r10k(master)
