503fcfdf0f831442d02b797d61de76df9b9473a9
Chris Price <chris@puppetlabs.com>
2016-08-05 14:35:07 -0700

    WIP: working on support for multiple PE versions

--- jenkins-integration/beaker/install/shared/60_classify_nodes_via_site_pp.rb
+++ jenkins-integration/beaker/install/shared/60_classify_nodes_via_site_pp.rb
@@ -14,12 +14,21 @@ end
 
 def classify_foss_nodes(host, nodes)
   environments = on(host, puppet('config print environmentpath')).stdout.chomp
+  # on old PEs (3.3 at least), this returns an empty string, so we'll hard-code
+  #  the path for now.  This may need to be changed to support more versions of
+  #  PE and/or OSS in the future.
+  if environments == ""
+    environments = '/etc/puppetlabs/puppet/environments'
+  end
   nodes = group_by_environment(nodes)
   nodes.each_pair do |env, node_configs|
-    sitepp = generate_sitepp(node_configs)
     manifestdir = "#{environments}/#{env}/manifests"
+    manifestfile = "#{manifestdir}/site.pp"
+    Beaker::Log.notify("Saving node configs for env '#{env}' to file '#{manifestfile}'")
+    sitepp = generate_sitepp(node_configs)
+
     on(host, "mkdir -p #{manifestdir}")
-    create_remote_file(host, "#{manifestdir}/site.pp", sitepp)
+    create_remote_file(host, manifestfile, sitepp)
     on(host, "chmod 644 #{manifestdir}/site.pp")
   end
 end
