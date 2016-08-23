752edcdb28434b44d3214e88da1432d14190731d
Chris Price <chris@puppetlabs.com>
2016-08-23 12:41:38 -0700

    WIP: working on installing OSS

--- jenkins-integration/beaker/install/foss/30_install_dev_repos.rb
+++ jenkins-integration/beaker/install/foss/30_install_dev_repos.rb
@@ -1,9 +1,27 @@
 install_opts = options.merge( { :dev_builds_repos => ["PC1"] })
 repo_config_dir = 'tmp/repo_configs'
 
+require 'net/http'
+
+def get_latest_master_version
+  response = Net::HTTP.get(URI('http://builds.puppetlabs.lan/puppetserver/?C=M&O=D'))
+
+  response.lines do |l|
+    next unless l =~ /<td><a /
+    next unless l =~ /master/
+    match = l.match(/^.*href="([^"]+)\/\?C=M&amp;O=D".*$/)
+    return match[1]
+  end
+
+end
+
 step "Setup Puppet Server repositories." do
   package_build_version = ENV['PACKAGE_BUILD_VERSION']
+  if package_build_version = "latest"
+    package_build_version = get_latest_master_version()
+  end
   if package_build_version
+    Beaker::Log.notify("Installing OSS Puppet Server version '#{package_build_version}'")
     install_puppetlabs_dev_repo master, 'puppetserver', package_build_version,
                                 repo_config_dir, install_opts
   else
