3ac6cdfbd8602655e12491b43f0a9094d024b474
Chris Price <chris@puppetlabs.com>
2016-08-23 13:10:50 -0700

    WIP: working on installing OSS

--- jenkins-integration/jenkins-jobs/common/scripts/job-steps/020_install_oss.sh
+++ jenkins-integration/jenkins-jobs/common/scripts/job-steps/020_install_oss.sh
@@ -17,7 +17,7 @@ ssh-add ${HOME}/.ssh/id_rsa
 
 bundle exec beaker \
         --config hosts.yaml \
-        --type aio
+        --type aio \
         --load-path lib \
         --log-level debug \
         --no-color \
