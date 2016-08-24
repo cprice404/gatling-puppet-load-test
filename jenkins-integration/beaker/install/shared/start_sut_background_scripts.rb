75152af37d9f250349ea5ee1e2ff430ea954a135
Chris Price <chris@puppetlabs.com>
2016-08-24 15:39:59 -0700

    WIP: working on bg scripts

--- jenkins-integration/beaker/install/shared/start_sut_background_scripts.rb
+++ jenkins-integration/beaker/install/shared/start_sut_background_scripts.rb
@@ -1,7 +1,12 @@
 step "Launch background scripts on SUT" do
   bg_scripts = ENV['SUT_BACKGROUND_SCRIPTS'].split("\n")
   Beaker::Log.notify("Launching #{bg_scripts.count} background scripts")
+  master_tempdir = master.tmpdir('gplt_bg_scripts')
   bg_scripts.each do |s|
+    script_filename = File.basename(s)
     Beaker::Log.notify("Launching script '#{s}'")
+    scp_to(master, s, master_tempdir)
+    on(host, "chmod 644 #{target_path}")
+    on(host, "#{File.join(master_tempdir, script_filename)} &")
   end
 end
