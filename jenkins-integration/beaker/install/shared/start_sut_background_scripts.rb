4ac8eb9e45bfe1abd3ac3ae8321ee23195cd3f7c
Chris Price <chris@puppetlabs.com>
2016-08-25 07:40:33 -0700

    WIP: working on bg scripts

--- jenkins-integration/beaker/install/shared/start_sut_background_scripts.rb
+++ jenkins-integration/beaker/install/shared/start_sut_background_scripts.rb
@@ -5,10 +5,12 @@ step "Launch background scripts on SUT" do
   bg_scripts.each do |s|
     script_filename = File.basename(s)
     remote_path = File.join(master_tempdir, script_filename)
+
     Beaker::Log.notify("Launching script '#{s}'")
     scp_to(master, s, master_tempdir)
     # on(master, "chmod 644 #{remote_path}")
-    result = on(master, "nohup #{remote_path} &")
+    # result = on(master, "nohup #{remote_path} &")
+    result = on(master, "#{remote_path} > #{master_tempdir}/stdout.log 2> #{master_tempdir}/stderr.log & echo $!")
     Beaker::Log.notify("GOT RESULT FROM SCRIPT EXECUTION: '#{result}'")
     Beaker::Log.notify("\tEXIT CODE: '#{result.exit_code}'")
     Beaker::Log.notify("\tSTDOUT: '#{result.stdout}'")
--- jenkins-integration/jenkins-jobs/common/scripts/background/echo_loop.sh
+++ jenkins-integration/jenkins-jobs/common/scripts/background/echo_loop.sh
@@ -3,9 +3,11 @@
 set -e
 set -x
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 COUNTER=0
 while true; do
-    echo "The counter is $COUNTER" >> echo.txt
+    echo "The counter is $COUNTER" >> "${DIR}/echo.txt"
     let COUNTER=COUNTER+1
     sleep 5
 done
