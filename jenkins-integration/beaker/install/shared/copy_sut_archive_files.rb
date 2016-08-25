f90c25acd40a6d7b6e422001405b913703b9b4d1
Chris Price <chris@puppetlabs.com>
2016-08-25 15:46:38 -0700

    WIP: working on archiving SUT files

--- jenkins-integration/beaker/install/shared/copy_sut_archive_files.rb
+++ jenkins-integration/beaker/install/shared/copy_sut_archive_files.rb
@@ -2,7 +2,7 @@ require 'fileutils'
 
 step "Copy archive files from SUT" do
   archive_files = ENV['SUT_ARCHIVE_FILES'].split("\n")
-  job_name = ENV['JOB_NAME']
+  job_name = ENV['PUPPET_GATLING_JOB_NAME']
   Beaker::Log.notify("Copying #{archive_files.count} archive files from SUT")
 
   FileUtils.mkdir_p("./sut_archive_files/#{job_name}")
