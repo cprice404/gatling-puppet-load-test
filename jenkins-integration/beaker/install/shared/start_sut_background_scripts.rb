b6a04c35a4d3701a3b49a3f97c0374465406adc8
Chris Price <chris@puppetlabs.com>
2016-08-25 07:52:16 -0700

    WIP: working on bg scripts

--- jenkins-integration/beaker/install/shared/start_sut_background_scripts.rb
+++ jenkins-integration/beaker/install/shared/start_sut_background_scripts.rb
@@ -1,7 +1,10 @@
+require 'json'
+
 step "Launch background scripts on SUT" do
   bg_scripts = ENV['SUT_BACKGROUND_SCRIPTS'].split("\n")
   Beaker::Log.notify("Launching #{bg_scripts.count} background scripts")
   master_tempdir = master.tmpdir('gplt_bg_scripts')
+  pids = {}
   bg_scripts.each do |s|
     script_filename = File.basename(s)
     remote_path = File.join(master_tempdir, script_filename)
@@ -15,6 +18,13 @@ step "Launch background scripts on SUT" do
     Beaker::Log.notify("\tEXIT CODE: '#{result.exit_code}'")
     Beaker::Log.notify("\tSTDOUT: '#{result.stdout}'")
     Beaker::Log.notify("\tSTDERR: '#{result.stderr}'")
-
+    pid = result.stdout.chomp
+    pids[s] = pid
   end
+
+  Beaker::Log.notify("All bg scripts launched; saving pids to bg_pids.json")
+  File.open("bg_pids.json", "w") do |f|
+    f.write(JSON.pretty_generate(pids))
+  end
+  Beaker::Log.notify(File.read(bg_pids.json))
 end
