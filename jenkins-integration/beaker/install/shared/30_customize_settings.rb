eade511f487eac310650c3a52b2f0d46dc3c8059
Chris Price <chris@puppetlabs.com>
2016-09-19 17:50:41 -0700

    WIP: added env var w/puppet settings JSON

---
+++ jenkins-integration/beaker/install/shared/30_customize_settings.rb
@@ -0,0 +1,15 @@
+require 'json'
+
+test_name 'Configure settings on SUT'
+
+def get_puppet_settings_from_env()
+  ENV['PUPPET_SERVER_PUPPET_SETTINGS']
+end
+
+# def update_service_config(host, source_path, service_name)
+#   scp_to(host, source_path, service_config_name(service_name))
+#   on(host, "chmod 644 #{service_config_name(service_name)}")
+# end
+
+puppet_settings = JSON.parse(get_puppet_settings_from_env())
+Beaker::Log.notify("TODO: INITIALIZE PUPPET SETTINGS to: #{puppet_settings}")
