a5564376bdf4633579e88bec5f427bde6a9967b8
Chris Price <chris@puppetlabs.com>
2016-09-26 16:46:09 -0700

    WIP: add logging if settings are skipped

--- jenkins-integration/jenkins-jobs/common/scripts/jenkins/pipeline.groovy
+++ jenkins-integration/jenkins-jobs/common/scripts/jenkins/pipeline.groovy
@@ -167,7 +167,9 @@ def step025_collect_facter_data(job_name, gatling_simulation_config, script_dir,
 }
 
 def step030_customize_settings(script_dir, puppet_settings) {
-    if (puppet_settings != null) {
+    if (puppet_settings == null) {
+        echo "Skipping settings customization; no overrides found in `puppet_settings`."
+    } else {
         puppet_settings_json = JsonOutput.toJson(puppet_settings)
         withEnv(["PUPPET_GATLING_PUPPET_SETTINGS=${puppet_settings_json}",
                  "PUPPET_SERVER_SERVICE_NAME=${server_era["service_name"]}"]) {
