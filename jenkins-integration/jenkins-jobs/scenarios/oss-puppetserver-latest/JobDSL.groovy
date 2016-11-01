fe1b72d9d2049e27b2dc60321990340160c0d876
Chris Price <chris@puppetlabs.com>
2016-11-01 10:19:12 -0700

    WIP: testing overriding params

--- jenkins-integration/jenkins-jobs/scenarios/oss-puppetserver-latest/JobDSL.groovy
+++ jenkins-integration/jenkins-jobs/scenarios/oss-puppetserver-latest/JobDSL.groovy
@@ -4,14 +4,24 @@ job.with {
         //  on Mondays.
         scm('H H(21-22) * * 1')
     }
-    parameters {
-        stringParam('SUT_HOST',
-                // TODO: eventually we need to come up with a better solution for
-                // managing the available SUTs to make sure that we don't try to
-                // use one of them for two jobs at the same time.  That will probably
-                // involve getting rid of the build parameter entirely, and using
-                // a jenkins plugin to request one and lock it during the run.
-                'puppetserver-perf-sut54.delivery.puppetlabs.net')
-        booleanParam('SKIP_PROVISIONING', false)
+//    parameters {
+//        stringParam('SUT_HOST',
+//                // TODO: eventually we need to come up with a better solution for
+//                // managing the available SUTs to make sure that we don't try to
+//                // use one of them for two jobs at the same time.  That will probably
+//                // involve getting rid of the build parameter entirely, and using
+//                // a jenkins plugin to request one and lock it during the run.
+//                'puppetserver-perf-sut54.delivery.puppetlabs.net')
+//        booleanParam('SKIP_PROVISIONING', false)
+//    }
+    configure { Node project ->
+        Node node = project / 'properties' / 'hudson.model.ParametersDefinitionProperty' / 'parameterDefinitions'
+        List children = node.children()
+        children.each { child ->
+            node.remove(child)
+        }
+//        context.buildParameterNodes.values().each {
+//            node << it
+//        }
     }
 }
