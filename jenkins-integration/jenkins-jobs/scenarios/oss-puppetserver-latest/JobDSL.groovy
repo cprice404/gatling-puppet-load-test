938482e8dbe15ad33eac2ecf16b89c95f72f1895
Chris Price <chris@puppetlabs.com>
2016-11-01 10:42:18 -0700

    WIP: testing overriding params

--- jenkins-integration/jenkins-jobs/scenarios/oss-puppetserver-latest/JobDSL.groovy
+++ jenkins-integration/jenkins-jobs/scenarios/oss-puppetserver-latest/JobDSL.groovy
@@ -17,7 +17,8 @@ job.with {
 //        booleanParam('SKIP_PROVISIONING', false)
 //    }
     configure { Node project ->
-        out.println("EXECUTING JOB CONFIGURE; project: ${project}")
+//        out.println("EXECUTING JOB CONFIGURE; project: ${project}")
+        out.println("EXECUTING JOB CONFIGURE")
 
         Node node = project / 'properties' / 'hudson.model.ParametersDefinitionProperty' / 'parameterDefinitions'
         List children = node.children().collect()
